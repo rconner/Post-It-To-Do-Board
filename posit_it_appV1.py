dd050d42433a848914519f7d54e42ac2963714f2
rconner <90843807+rconner@users.noreply.github.com>
2022-11-20 15:50:50 -0500

    add final touches
    Added title bar and changed the font colors and background colors

--- posit_it_appV1.py
+++ posit_it_appV1.py
@@ -1,10 +1,17 @@
 from tkinter import *
+from tkinter import ttk
 
 
 root = Tk()
 #Create frame in TK
 frame = Frame(root, width=500, height=400)
+
+
 frame.place()
+root.title("Post It To Do's")
+
+
+font_tuple = ("Great Vibes", 9, "normal")
 
 # create buttons under row within grid - adjust padding for smaller buttons. row 0 col 0 widget row 1 column 0 button
 #make clear method for each button
@@ -38,40 +45,52 @@ def delete_text12():
 #Create text box dimensions
 my_text1 = Text(root, width=13, height=7, wrap="word")
 my_text1.grid(row=0, column=0, padx=10, pady=10)
-text_1_button = Button(root, width=3, height=1, text = "Clear", command=delete_text1).grid(row=1, column=0, padx=2, pady=2)
+my_text1.configure(font = font_tuple, fg = 'cyan', bg = 'black')
+text_1_button = Button(root, width=4, height=1, text = "Clear", command=delete_text1).grid(row=1, column=0, padx=2, pady=2)
 my_text2 = Text(root, width=13, height=7, wrap="word")
 my_text2.grid(row=0, column=1, padx=10, pady=10)
-text_2_button = Button(root, width=3, height=1, text = "Clear", command=delete_text2).grid(row=1, column=1, padx=2, pady=2)
+my_text2.configure(font = font_tuple, fg = 'yellow', bg = 'black')
+text_2_button = Button(root, width=4, height=1, text = "Clear", command=delete_text2).grid(row=1, column=1, padx=2, pady=2)
 my_text3 = Text(root, width=13, height=7, wrap="word")
 my_text3.grid(row=0, column=2, padx=10, pady=10)
-text_3_button = Button(root, width=3, height=1, text = "Clear", command=delete_text3).grid(row=1, column=2, padx=2, pady=2)
+my_text3.configure(font = font_tuple, fg = 'magenta', bg = 'black')
+text_3_button = Button(root, width=4, height=1, text = "Clear", command=delete_text3).grid(row=1, column=2, padx=2, pady=2)
 my_text4 = Text(root, width=13, height=7, wrap="word")
 my_text4.grid(row=2, column=0, padx=10, pady=10)
-text_4_button = Button(root, width=3, height=1, text = "Clear", command=delete_text4).grid(row=3, column=0, padx=2, pady=2)
+my_text4.configure(font = font_tuple, fg = 'magenta', bg = 'black')
+text_4_button = Button(root, width=4, height=1, text = "Clear", command=delete_text4).grid(row=3, column=0, padx=2, pady=2)
 my_text5 = Text(root, width=13, height=7, wrap="word")
 my_text5.grid(row=2, column=1, padx=10, pady=10)
-text_5_button = Button(root, width=3, height=1, text = "Clear", command=delete_text5).grid(row=3, column=1, padx=2, pady=2)
+my_text5.configure(font = font_tuple, fg = 'cyan', bg = 'black')
+text_5_button = Button(root, width=4, height=1, text = "Clear", command=delete_text5).grid(row=3, column=1, padx=2, pady=2)
 my_text6 = Text(root, width=13, height=7, wrap="word")
 my_text6.grid(row=2, column=2, padx=10, pady=10)
-text_6_button = Button(root, width=3, height=1, text = "Clear", command=delete_text6).grid(row=3, column=2, padx=2, pady=2)
+my_text6.configure(font = font_tuple, fg = 'yellow', bg = 'black')
+text_6_button = Button(root, width=4, height=1, text = "Clear", command=delete_text6).grid(row=3, column=2, padx=2, pady=2)
 my_text7 = Text(root, width=13, height=7, wrap="word")
 my_text7.grid(row=4, column=0, padx=10, pady=10)
-text_7_button = Button(root, width=3, height=1, text = "Clear", command=delete_text7).grid(row=5, column=0, padx=2, pady=2)
+my_text7.configure(font = font_tuple, fg = 'yellow', bg = 'black')
+text_7_button = Button(root, width=4, height=1, text = "Clear", command=delete_text7).grid(row=5, column=0, padx=2, pady=2)
 my_text8 = Text(root, width=13, height=7, wrap="word")
 my_text8.grid(row=4, column=1, padx=10, pady=10)
-text_8_button = Button(root, width=3, height=1, text = "Clear", command=delete_text8).grid(row=5, column=1, padx=2, pady=2)
+my_text8.configure(font = font_tuple, fg = 'magenta', bg = 'black')
+text_8_button = Button(root, width=4, height=1, text = "Clear", command=delete_text8).grid(row=5, column=1, padx=2, pady=2)
 my_text9 = Text(root, width=13, height=7, wrap="word")
 my_text9.grid(row=4, column=2, padx=10, pady=10)
-text_9_button = Button(root, width=3, height=1, text = "Clear", command=delete_text9).grid(row=5, column=2, padx=2, pady=2)
+my_text9.configure(font = font_tuple, fg = 'cyan', bg = 'black')
+text_9_button = Button(root, width=4, height=1, text = "Clear", command=delete_text9).grid(row=5, column=2, padx=2, pady=2)
 my_text10 = Text(root, width=13, height=7, wrap="word")
 my_text10.grid(row=6, column=0, padx=10, pady=10)
-text_10_button = Button(root, width=3, height=1, text = "Clear", command=delete_text10).grid(row=7, column=0, padx=2, pady=2)
+my_text10.configure(font = font_tuple, fg = 'cyan', bg = 'black')
+text_10_button = Button(root, width=4, height=1, text = "Clear", command=delete_text10).grid(row=7, column=0, padx=2, pady=2)
 my_text11 = Text(root, width=13, height=7, wrap="word")
 my_text11.grid(row=6, column=1, padx=10, pady=10)
-text_11_button = Button(root, width=3, height=1, text = "Clear", command=delete_text11).grid(row=7, column=1, padx=2, pady=2)
+my_text11.configure(font = font_tuple, fg = 'yellow', bg = 'black')
+text_11_button = Button(root, width=4, height=1, text = "Clear", command=delete_text11).grid(row=7, column=1, padx=2, pady=2)
 my_text12 = Text(root, width=13, height=7, wrap="word")
 my_text12.grid(row=6, column=2, padx=10, pady=10)
-text_12_button = Button(root, width=3, height=1, text = "Clear", command=delete_text12).grid(row=7, column=2, padx=2, pady=2)
+my_text12.configure(font = font_tuple, fg = 'magenta', bg = 'black')
+text_12_button = Button(root, width=4, height=1, text = "Clear", command=delete_text12).grid(row=7, column=2, padx=2, pady=2)
 
 
 mainloop()
